0d1823f2acca4d5d5bbb10310567313a6866038d
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2020-04-13 22:44:53 +0200

    nicer board scheme

--- demo-2/src/arduino-app/arduino/arduino.ino
+++ demo-2/src/arduino-app/arduino/arduino.ino
@@ -4,7 +4,7 @@ int RedLightPin = 7;
 int YellowLightPin = 6;
 int GreenLightPin = 5;
 int BuzzerPin = 10;
-int photoResistorPin = A0;
+int ServoPin = 9;
 
 Servo servo1;
 
@@ -17,9 +17,7 @@ void setup() {
   
   pinMode(BuzzerPin, OUTPUT);
   
-  servo1.attach(9);
-
-  pinMode(photoResistorPin, INPUT);
+  servo1.attach(ServoPin);
 
   // Initial test
   executeCommand('R', '1', NULL);
@@ -42,14 +40,8 @@ void loop()
   char device;
   char command;
   char * params;  
-  int value;
-  bool newCommand = false;
 
   getCommand(&device, &command, &params);
- 
-  value = analogRead(photoResistorPin);
-    
-
   executeCommand(device, command, params);
   free(params);
 }
