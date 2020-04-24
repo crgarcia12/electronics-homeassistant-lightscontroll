0b4ceb90379992966131950fc7f7b1119ece4244
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2020-04-25 01:45:33 +0200

    second demo

--- demo-2/src/arduino-app/arduino/arduino.ino
+++ demo-2/src/arduino-app/arduino/arduino.ino
@@ -1,219 +1,114 @@
-#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
-int RedLightPin = 7;
-int YellowLightPin = 6;
-int GreenLightPin = 5;
-int BuzzerPin = 10;
-int ServoPin = 9;
+// Pin definitions
+const int encoderPinA = 12;
+const int encoderPinB = 11;
+const int soilHumiditySensor = A0;
 
-Servo servo1;
+// Setting up the humidity Counter
+int reading = 0;
+int lowest = 0;
+int highest = 32000;
+int changeamnt = 1;
+
+// Timing for polling the encoder
+unsigned long currentTime;
+unsigned long lastTimeEncoder;
+unsigned long lastPrintedMilis;
+
+// Storing the readings
+boolean encoderA;
+boolean encoderB;
+boolean lastEncoderA = false;
+
+//initialize the liquid crystal library
+//the first parameter is the I2C address
+//the second parameter is how many rows are on your screen
+//the third parameter is how many columns are on your screen
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+String desiredValueStr = "Desired: ";
+String currentValueStr = "Value: ";
+String concatStr = "";
 
 void setup() {
+  // set the encoder pins
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
+  
+  // Set up the timings for encoder and display
+  lastPrintedMilis = currentTime = millis();
+  lastTimeEncoder = currentTime; 
+
+  // Soil sensor
+  pinMode(soilHumiditySensor, INPUT);
+  
+  // Start the serial monitor for debugging
   Serial.begin(9600);
-  
-  pinMode(RedLightPin, OUTPUT);
-  pinMode(YellowLightPin, OUTPUT);
-  pinMode(GreenLightPin, OUTPUT);  
-  
-  pinMode(BuzzerPin, OUTPUT);
-  
-  servo1.attach(ServoPin);
 
-  // Initial test
-  executeCommand('R', '1', NULL);
-  executeCommand('Y', '1', NULL);
-  executeCommand('G', '1', NULL);
-  executeCommand('M', 'x', "180");
-  executeCommand('B', '1', "1000");
+  //initialize lcd screen
+  lcd.init();
+  // turn on the backlight
+  lcd.backlight();
+} 
 
-  delay(1000);
-  
-  executeCommand('R', '0', NULL);
-  executeCommand('Y', '0', NULL);
-  executeCommand('G', '0', NULL);
-  executeCommand('M', 'x', "0");
-  executeCommand('B', '0', NULL); 
-}
 
-void loop() 
+void loop()
 { 
-  char device;
-  char command;
-  char * params;  
-
-  getCommand(&device, &command, &params);
-  executeCommand(device, command, params);
-  free(params);
-}
-
-void executeCommand(const char device, const char command, const char * params)
-{
-  // Aux variables
-  int motorAngle = 0;
-  int buzzerTone = 0;
+  int humidityValue = analogRead(soilHumiditySensor);
   
-  PrintSerial("[DBG: Start 2processing: ", device, "-", command, "-", params, "]\0");
-  switch(device) {
-    case 'B':
-      buzzerTone = ((String)params).toInt();
-      if(command == '0')
-      {
-        noTone(BuzzerPin);
-      }
-      else
-      {
-        tone(BuzzerPin, buzzerTone); 
-      }
-      break;
-    case 'R':
-      if(command == '1')
-      {
-        digitalWrite(RedLightPin, HIGH);
-      }
-      else
-      {
-        digitalWrite(RedLightPin, LOW);
-      }
-      break;
-    case 'Y':
-      if(command == '1')
-      {
-        digitalWrite(YellowLightPin, HIGH);
-      }
-      else
-      {
-        digitalWrite(YellowLightPin, LOW);
-      }
-      break;
-    case 'G':
-      if(command == '1')
-      {
-        digitalWrite(GreenLightPin, HIGH);
-      }
-      else
-      {
-        digitalWrite(GreenLightPin, LOW);
-      }
-      break;
-    case 'M':
-      motorAngle = ((String)params).toInt();
-      PrintSerial("[DBG: Motor angle send: ", motorAngle, "]\0");
-      servo1.write(motorAngle);
-      break;
-  }
-
-  PrintSerial("[DBG: Finished processing: ", device, "-", command, "-", params, "]\0");
-}
-
-void getCommand(char * device, char * command, char * * paramsPtr)
-{
-  *paramsPtr  = (char*) malloc(sizeof(char) * 8);
-  char * params  = * paramsPtr;
-  params[0] = '\0';
-  
-  char entireCommandString[12];
-  
-  char incommingChar;
-  int charIndex = 0;
-  bool commandInitiated = false;
-  
-  while (true) 
+  // Read elapsed time
+  currentTime = millis();
+ 
+  if (currentTime >= (lastPrintedMilis + 200))
   {
-    // wait for the char
-    while (!Serial.available()) {}
-    incommingChar = Serial.read();
-
-    PrintSerial("[DBG: Command char arrived. Char='", incommingChar, "'. CharIndex='", charIndex, "']");
-
-    // Until we dont get a '[' we don't react
-    if (!commandInitiated)
-    {
-      if(incommingChar != '[')
-      {
-        continue;
-      }
-      commandInitiated = true;
-    }
-
-    // Did the command finished?
-    if (incommingChar == ']' || charIndex > 10)
-    {
-      entireCommandString[charIndex++] = incommingChar;
-      break;
-    }
-
-    // Where do we put the received char?
-    if(charIndex < 1)
-    {
-      // This was the opening braket, do nothing
-    }
-    else if(charIndex < 2)
-    {
-      (*device) = incommingChar;
-    }
-    else if(charIndex < 3)
-    {
-      (*command) = incommingChar;
-    }
-    else
-    {
-      params[charIndex - 3] = incommingChar;
-    } 
-
-    entireCommandString[charIndex] = incommingChar;
-    charIndex++;
+    // tell the screen to write on the top row
+    lcd.setCursor(0,0);
+    // tell the screen to write “hello, from” on the top row
+    concatStr = desiredValueStr + reading;
+    lcd.print(concatStr);
+    // tell the screen to write on the bottom row
+    lcd.setCursor(0,1);
+    // tell the screen to write “Arduino_uno_guy” on the bottom row
+    // you can change whats in the quotes to be what you want it to be!
+    concatStr = currentValueStr + humidityValue;
+    lcd.print(concatStr);
+    lastPrintedMilis = currentTime;
   }
 
-  // This one is -4 because the last char (']') incremented charIndex
-  params[charIndex - 4] = '\0';
-  entireCommandString[charIndex++] = '\0';
-  
-  
-  // Confirmation of what we are going to process
-  PrintSerial("[confirm:", entireCommandString);
-}
-
-void PrintSerial(const char* str1, const char* str2)
-{
-  Serial.print(str1);
-  Serial.print(str2);
-}
-
-void PrintSerial(const char* str1, int int1, const char* str3)
-{
-  Serial.print(str1);
-  Serial.print(int1);
-  Serial.print(str3);
-}
-
-void PrintSerial(
-  const char* str1,
-  const char char2,
-  const char* str3,
-  const int int4,
-  const char * str5)
-{
-  Serial.print(str1);
-  Serial.print(char2);
-  Serial.print(str3);
-  Serial.print(int4, DEC);
-  Serial.print(str5);
-}
-
-void PrintSerial(
-  const char* str1,
-  const char char2,
-  const char* str3,
-  const char char4,
-  const char * str5,
-  const char * str6,
-  const char * str7)
-{
-  Serial.print(str1);
-  Serial.print(char2);
-  Serial.print(str3);
-  Serial.print(char4);
-  Serial.print(str5);
-  Serial.print(str6);
-  Serial.print(str7);
+  // Check if it's time to read the encoder
+  if (currentTime >= (lastTimeEncoder + 2))
+  {
+    // read the two pins
+    encoderA = digitalRead(encoderPinA);
+    encoderB = digitalRead(encoderPinB);
+    // check if A has gone from high to low
+    if ((!encoderA) && (lastEncoderA))
+    {
+      // check if B is high
+      if (encoderB)
+      {
+        // clockwise
+        if (reading + changeamnt <= highest)
+        {
+          reading = reading + changeamnt; 
+        }
+      }
+      else
+      {
+        // anti-clockwise
+        if (reading - changeamnt >= lowest)
+        {
+          reading = reading - changeamnt; 
+        }
+      }
+      // Output reading for debugging
+      Serial.println(reading);
+    }
+    
+    // store reading of A and millis for next loop
+    lastEncoderA = encoderA;
+    lastTimeEncoder = currentTime;
+  }
 }
