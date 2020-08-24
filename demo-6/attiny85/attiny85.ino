eab9c92d1560690ed60885432c3aa550c72f4474
crgarcia12 <crgarcia@live.com.ar>
2020-08-25 01:09:22 +0200

    adding lcd

--- demo-6/attiny85/attiny85.ino
+++ demo-6/attiny85/attiny85.ino
@@ -8,9 +8,20 @@
    4. Tools -> Programmer: Arduino as ISP (ATTiny Core)
    5. Tools -> Clock -> 8Mhz Internal
    6. Burn bootloader (sounds scary)
+
+   7. Install Library HCSR04 by Martin Soic:https://github.com/Martinsos/arduino-lib-hc-sr04
+   8. Install Library LiquidCrystal_I2C
 */
 
+// Installed libraries
+#include <LiquidCrystal_I2C.h>
 #include <HCSR04.h>
+
+// Arduino Internal Libraries
+#include <Wire.h> 
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 
 #define EchoPin 1
 #define TrigPin 2
@@ -30,10 +41,19 @@ int heartBeatStatus = LOW;
 // Create and initialize the Ultrasonic object.
 UltraSonicDistanceSensor distanceSensor(TrigPin, EchoPin);
 
+String DistanceStr = "Distancia: ";
+String PumpStateStr = "Bomba: ";
+String concatStr = "";
+
 void setup() 
 {
   pinMode(RelayPin, OUTPUT);
   pinMode(HeartBeatPin, OUTPUT); 
+
+  //initialize lcd screen
+  lcd.init();
+  // turn on the backlight
+  lcd.backlight();
 }
 
 void loop()
@@ -60,7 +80,16 @@ void loop()
   {
     heartBeatStatus = LOW;
   }
+
   digitalWrite(HeartBeatPin, heartBeatStatus);
+
+  concatStr = DistanceStr + distance;
+  lcd.setCursor(0,0);
+  lcd.print(concatStr);
+
+  concatStr = PumpStateStr + pumpSatus;
+  lcd.setCursor(0,1);
+  lcd.print(concatStr);
 
   delay(1000);
 
