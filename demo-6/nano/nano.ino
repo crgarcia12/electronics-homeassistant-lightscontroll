3e7fdc2f7989ab3ab6ff8d90f0b5a9827b5fbedc
crgarcia12 <crgarcia@live.com.ar>
2020-09-04 12:56:30 +0200

    fixing encoder

--- demo-6/nano/nano.ino
+++ demo-6/nano/nano.ino
@@ -1,11 +1,14 @@
 /*
    1. Tools -> Board -> Arduino nano
    2. Tools -> Processor -> ATmega 328P (no bootloader)
+   3. Tools -> Port /dev/ttyUSB0 (Linux) (Try another usb port if it does not work - Lenovo is the right one)
    
-   3. Install Library HCSR04 by Martin Soic:https://github.com/Martinsos/arduino-lib-hc-sr04
-   4. Install Library LiquidCrystal_I2C
-   5. Encoder code from: https://www.instructables.com/id/Improved-Arduino-Rotary-Encoder-Reading/
+   4. Install Library HCSR04 by Martin Soic:https://github.com/Martinsos/arduino-lib-hc-sr04
+   5. Install Library LiquidCrystal_I2C
+   6. Encoder code from: https://www.instructables.com/id/Improved-Arduino-Rotary-Encoder-Reading/
 */
+
+//#define SerialDebug
 
 // Installed libraries
 #include <LiquidCrystal_I2C.h>
@@ -43,7 +46,7 @@ int pumpSatus = PumpOff;
 
 
 // Refresh
-#define UserRefreshTime 300
+#define UserRefreshTime 200
 
 // Display
 #define DisplaySize 16
@@ -69,7 +72,7 @@ enum MenuStates MenuCurrentState;
 // Encoder
 volatile byte aFlag = 0;
 volatile byte bFlag = 0;
-volatile byte encoderPos = 0;
+volatile int encoderPos = 0;
 volatile byte reading = 0;
 
 void setup() 
@@ -95,8 +98,10 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(EncoderPinA), EncoderPinAChanged, RISING);
   attachInterrupt(digitalPinToInterrupt(EncoderPinB), EncoderPinBChanged, RISING);
 
-  //Serial.begin(9600);
-  //Serial.println(debugCounter); debugCounter = debugCounter + 1;
+#ifdef SerialDebug
+  Serial.begin(9600);
+  Serial.println("Starting"); 
+#endif
 }
 
 void loop()
@@ -111,12 +116,10 @@ void loop()
     lastRefresh = millis();
   }
 
-
   /******************************** 
    *      Range calculation 
    *********************************/
   int distance = distanceSensor.measureDistanceCm();
-
   
   if (distance >= pumpStartDistance)
   {
@@ -128,37 +131,41 @@ void loop()
     pumpSatus = PumpOff;
     digitalWrite(RelayPin, LOW);
   }
-  
+
+  /******************************** 
+  *   Only during refresh cycles
+  *********************************/
+  if(!executeRefresh) 
+  {
+    return;
+  }
+
   /******************************** 
   *          HeartBeat led 
   *********************************/
-  if(executeRefresh) {
-    if (heartBeatStatus == LOW)
-    {
-      heartBeatStatus = HIGH;
-    }
-    else
-    {
-      heartBeatStatus = LOW;
-    }
-    digitalWrite(LED_BUILTIN, heartBeatStatus); 
-    digitalWrite(HeartBeatPin, heartBeatStatus);
+  if (heartBeatStatus == LOW)
+  {
+    heartBeatStatus = HIGH;
   }
+  else
+  {
+    heartBeatStatus = LOW;
+  }
+  digitalWrite(LED_BUILTIN, heartBeatStatus); 
+  digitalWrite(HeartBeatPin, heartBeatStatus);
   
   /******************************** 
    *          Menu 
    *********************************/
-  if(executeRefresh){
-    if (digitalRead(MenuButtonPin) == HIGH) {
-      MenuCurrentState = MenuCurrentState + 1;
-      encoderPos = 0;
-      if(MenuCurrentState >= LastItem)
-      {
-        MenuCurrentState = Main;
-      }
+  if (digitalRead(MenuButtonPin) == HIGH) {
+    MenuCurrentState = MenuCurrentState + 1;
+    encoderPos = 0;
+    if(MenuCurrentState >= LastItem)
+    {
+      MenuCurrentState = Main;
     }
   }
-
+  
   switch (MenuCurrentState){
     case Main:
       PumpStateStr[7] = '0' + pumpSatus;
@@ -174,33 +181,34 @@ void loop()
       if (pumpStopDistance > pumpStartDistance) pumpStopDistance = pumpStartDistance - 1;
       break;
   }
-  
+#ifdef SerialDebug
+  Serial.println(encoderPos);
+#endif
+  encoderPos = 0;
+
   /******************************** 
    *          Display 
    *********************************/
-  if(executeRefresh)
-  {
-    switch (MenuCurrentState){
-        case Main:
-          displayLine2 = PumpStateStr;
-          break;
-        case MaxDistance:
-          printNumbersInStr(PumpStartStr, pumpStartDistance, DisplaySize);
-          displayLine2 = PumpStartStr;
-          break;
-        case MinDistance:
-          printNumbersInStr(PumpStopStr, pumpStopDistance, DisplaySize);
-          displayLine2 = PumpStopStr;
-          break;
-    }
-
-    printNumbersInStr(DistanceStr, distance, DisplaySize);
-    lcd.setCursor(0,0);
-    lcd.print(DistanceStr);
-  
-    lcd.setCursor(0,1);
-    lcd.print(displayLine2);
+  switch (MenuCurrentState){
+      case Main:
+        displayLine2 = PumpStateStr;
+        break;
+      case MaxDistance:
+        printNumbersInStr(PumpStartStr, pumpStartDistance, DisplaySize);
+        displayLine2 = PumpStartStr;
+        break;
+      case MinDistance:
+        printNumbersInStr(PumpStopStr, pumpStopDistance, DisplaySize);
+        displayLine2 = PumpStopStr;
+        break;
   }
+
+  printNumbersInStr(DistanceStr, distance, DisplaySize);
+  lcd.setCursor(0,0);
+  lcd.print(DistanceStr);
+
+  lcd.setCursor(0,1);
+  lcd.print(displayLine2);
 }
 
 void printNumbersInStr (char *str, int number, int startPosition)
@@ -234,7 +242,7 @@ void EncoderPinAChanged(){
   //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
   if(reading == B00001100 && aFlag) 
   { 
-    encoderPos --; //decrement the encoder's position count
+    encoderPos--; //decrement the encoder's position count
     bFlag = 0; //reset flags for the next turn
     aFlag = 0; //reset flags for the next turn
   }
@@ -250,14 +258,14 @@ void EncoderPinAChanged(){
 void EncoderPinBChanged(){
   //stop interrupts happening before we read pin values
   cli();
-  
+
   //read all eight pin values then strip away all but pinA and pinB's values
   reading = PIND & 0xC;
   
   //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
   if (reading == B00001100 && bFlag)
   {
-    encoderPos ++; //increment the encoder's position count
+    encoderPos++; //increment the encoder's position count
     bFlag = 0; //reset flags for the next turn
     aFlag = 0; //reset flags for the next turn
   }
